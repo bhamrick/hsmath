a045f32d1fd7afe150bcbcf4466184a4caa229c8
Brian Hamrick <brian.c.hamrick@gmail.com>
2015-06-14 03:32:36 -0700

    Find small integer roots

--- Math/Polynomial/Coppersmith.hs
+++ Math/Polynomial/Coppersmith.hs
@@ -1,12 +1,22 @@
 module Math.Polynomial.Coppersmith where
 
+import Debug.Trace
+
+import Control.Monad.Random
+import Data.List
+import Data.Maybe
 import Data.Ratio
 import Math.Lattice.LLL (lll)
 import Math.Polynomial
+import Math.Polynomial.Hensel
+import Math.Polynomial.Modular
+import Math.Polynomial.ModularRoots
+import Math.Primes
+import System.Random
 
 -- TODO: Figure out what a good bound B is
-smallRoots :: P Integer -> Integer -> [Integer]
-smallRoots p n = undefined
+smallModularRoots :: P Integer -> Integer -> [Integer]
+smallModularRoots p n = undefined
 
 reducedPolynomial :: P Integer -> Integer -> Integer -> Int -> P Rational
 reducedPolynomial p n b h = evaluate (fmap constPoly (P (head reducedLatticeBasis))) (1 % b .* polyVar)
@@ -16,3 +26,25 @@ reducedPolynomial p n b h = evaluate (fmap constPoly (P (head reducedLatticeBasi
     -- polynomialBasis = (1 % n) .* fmap fromInteger p : [polyVar^i | i <- [0..d-1]]
     latticeBasis = [coefficients $ evaluate (fmap constPoly q) (fromInteger b * polyVar) | q <- polynomialBasis]
     reducedLatticeBasis = lll latticeBasis
+
+-- |smallRoots f b computes the integer roots with magnitude at most b
+smallRoots :: P Integer -> Integer -> [Integer]
+smallRoots f b = sort $ filter (\x -> evaluate f x == 0) rootCandidates
+    where
+    p :: Integer
+    p = evalRand (findUsablePrime 2) (mkStdGen 1)
+    findUsablePrime :: MonadRandom m => Int -> m Integer
+    findUsablePrime k = do
+        candidate <- randomPrime k
+        let g = modularPolyGcd (reducePoly f candidate) (reducePoly (derivative f) candidate) candidate
+        if degree g == 1
+            then return candidate
+            else findUsablePrime (k+1)
+    liftResidue :: Integer -> [Integer]
+    liftResidue = liftResidue' 1
+    liftResidue' :: Int -> Integer -> [Integer]
+    liftResidue' r x = if p^r > b
+        then [x, x - p^r]
+        else maybeToList (henselLift f x p r) >>= liftResidue' (2*r)
+    rootCandidates :: [Integer]
+    rootCandidates = rootsModP f p >>= liftResidue
